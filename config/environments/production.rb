ea34eae880e4ae13f71f398bafb852b9bbb6b6b5
Yaw Ofosu Owusu-Amoako <yoamoako@gmail.com>
2017-03-07 10:35:35 +0000

    Setup Contact Form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
     
+    config.action_mailer.default_url_options = {host: 'https://dashboard.heroku.com/apps/rocky-plateau-79043'}
     
 
 end
