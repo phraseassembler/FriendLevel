def7a03bd373544ffefeca4dc331c7257541ffd1
Yaw Ofosu Owusu-Amoako <yoamoako@gmail.com>
2017-03-07 10:27:51 +0000

    Project

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,17 @@
 class ContactsController < ApplicationController
     def new
-        @contact = Contact.new
-    end
+    @contact = Contact.new
+    end 
     
-    def create
+    def new
         @contact = Contact.new(params[:contact])
         @contact.request = request
         if @contact.deliver
             flash.now[:error] = nil
-        else
-            flash.now[:error] = 'Cannot Send Message.'
+            else
+            flash.now[:error] = 'Cannot send message.'
             render :new
         end 
     end
+    
 end
